ceca24b75c6bb127f6251bfc87b6cd980a71445a
Dennis 03 <adennis9993@gmail.com>
2020-08-04 23:47:00 +0530

    projects added

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-// alert("dfsg");
+var $grid = $(".grid").isotope({
+  // options
+  itemSelector: ".grid-item",
+  layoutMode: "fitRows",
+});
+
+// change is-checked class on buttons
+var $buttonGroup = $(".filters");
+$buttonGroup.on("click", "li", function (event) {
+  $buttonGroup.find(".is-checked").removeClass("is-checked");
+  var $button = $(event.currentTarget);
+  $button.addClass("is-checked");
+  var filterValue = $button.attr("data-filter");
+  $grid.isotope({ filter: filterValue });
+});
